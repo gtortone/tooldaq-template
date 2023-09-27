2e440ef61f1aca0f3c0362f6fd95698109ef5e98
Gennaro Tortone <gennaro.tortone@na.infn.it>
2023-09-27 17:35:44 +0200

    add blank tool MyFirstTool and test ToolChain

---
+++ UserTools/Unity.h
@@ -0,0 +1,1 @@
+#include "MyFirstTool.h"
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include "ToolChain.h"
+
+int main(int argc, char* argv[]) {
+
+   ToolChain tools("configfiles/MyToolChain/ToolChainConfig");
+
+   return 0;
+}
